ccaece694a584083b14f6791371e614475f2ed5b
Yunus Serhat Bıçakçı <yunus.serhat@marmara.edu.tr>
2022-06-29 19:23:11 +0300

    @day 2

--- solutions.py
+++ solutions.py
@@ -4,3 +4,14 @@ print('Hello, World.')
 print(input_string)
 
 #Day 2
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+i_ys = int(input())
+d_ys = float(input())
+s_ys = input()
+
+print(i + i_ys)
+print(d + d_ys)
+print(s + s_ys)
