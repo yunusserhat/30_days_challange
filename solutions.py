47567b4d2c72372d74fc3deddeb2039eceb4f316
Yunus Serhat Bıçakçı <yunus.serhat@marmara.edu.tr>
2022-06-30 21:44:34 +0300

    @day 2

--- solutions.py
+++ solutions.py
@@ -1,9 +1,9 @@
-#Day 1
+#Day 0
 input_string = input()
 print('Hello, World.')
 print(input_string)
 
-#Day 2
+#Day 1
 i = 4
 d = 4.0
 s = 'HackerRank '
@@ -15,3 +15,26 @@ s_ys = input()
 print(i + i_ys)
 print(d + d_ys)
 print(s + s_ys)
+
+#Day 2
+
+import math
+import os
+import random
+import re
+import sys
+
+def solve(meal_cost, tip_percent, tax_percent):
+
+    total_cost =  meal_cost + meal_cost * tip_percent/100 + meal_cost * tax_percent/100
+    print(round(total_cost))
+
+if __name__ == '__main__':
+    meal_cost = float(input())
+
+    tip_percent = int(input())
+
+    tax_percent = int(input())
+
+    solve(meal_cost, tip_percent, tax_percent)
+
